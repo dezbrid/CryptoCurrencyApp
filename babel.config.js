c67289d6c27b5f4a404ac7b56b127442262ee18f
Dezbrid <dezbrid@gmail.com>
2023-08-11 00:02:07 -0500

    initial structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'import-glob',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@types': './src/types',
+          '@screens': './src/screens',
+          '@constants': './src/constants',
+          '@routes': './src/routes',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+import styles from './styles';
+
+export function Home() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>HOME</Text>
+    </SafeAreaView>
+  );
+}
